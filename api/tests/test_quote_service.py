98882778a46501d1acb4939cb4a79071bd198cba
Willian Silva <williansilva@alu.ufc.br>
2026-03-28 12:42:28 -0300

    feat: Refactor Create Quote API and enhance test structure with valid request data fixture

--- api/tests/test_quote_service.py
+++ api/tests/test_quote_service.py
@@ -18,9 +18,9 @@ def mock_repo():
     return AsyncMock(spec=QuoteRepository)
 
 
-@pytest.mark.asyncio
-async def test_create_quote_sucess(service, mock_repo):
-    request_data = CreateQuoteRequest(
+@pytest.fixture
+def valid_request_data():
+    return CreateQuoteRequest(
         content="Investir é sobre ter paciência.",
         author="Warren Buffett",
         tags=[CategoryQuote.INVESTIMENTOS, CategoryQuote.EDUCACAO],
@@ -28,22 +28,26 @@ async def test_create_quote_sucess(service, mock_repo):
         verified=True,
     )
 
-    quote = Quote.model_construct(
-        content=request_data.content,
-        author=request_data.author,
-        tags=request_data.tags,
-        source=request_data.source,
-        verified=request_data.verified,
-    )
 
-    mock_repo.create.return_value = quote
+class TestCreateQuote:
+    @pytest.mark.asyncio
+    async def test_create_quote_sucess(self, service, mock_repo, valid_request_data):
+        quote = Quote.model_construct(
+            content=valid_request_data.content,
+            author=valid_request_data.author,
+            tags=valid_request_data.tags,
+            source=valid_request_data.source,
+            verified=valid_request_data.verified,
+        )
 
-    result = await service.create_quote(quote=request_data, repo=mock_repo)
+        mock_repo.create.return_value = quote
 
-    assert result.content == request_data.content
-    assert result.author == request_data.author
-    assert result.tags == request_data.tags
-    assert result.source == request_data.source
-    assert result.verified == request_data.verified
+        result = await service.create_quote(quote=valid_request_data, repo=mock_repo)
 
-    mock_repo.create.assert_called_once_with(request_data)
+        assert result.content == valid_request_data.content
+        assert result.author == valid_request_data.author
+        assert result.tags == valid_request_data.tags
+        assert result.source == valid_request_data.source
+        assert result.verified == valid_request_data.verified
+
+        mock_repo.create.assert_called_once_with(valid_request_data)
