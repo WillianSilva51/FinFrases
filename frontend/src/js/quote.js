f02b485a5293bd3974f18159b100f88471519814
Willian Silva <williansilva@alu.ufc.br>
2026-07-15 23:01:24 -0300

    feat: enhance quote display functionality; add source and tags to quotes; improve CSS styles for better layout

--- frontend/src/js/quote.js
+++ frontend/src/js/quote.js
@@ -7,12 +7,34 @@ const randomQuoteButton = document.getElementById("random-quote-button");
 const quoteTitle = document.getElementById("quote-title");
 const quoteText = document.getElementById("quote-text");
 const quoteAuthor = document.getElementById("quote-author");
+const quoteSource = document.getElementById("quote-source");
+const quoteTags = document.getElementById("quote-tags");
 const validTypes = new Set(["today", "random"]);
+
+const validQuotesTags = {
+    "GERAL": ["bg-gray-700", "text-white"],
+    "INVESTIMENTOS": ["bg-green-700", "text-white"],
+    "POUPANCA": ["bg-blue-700", "text-white"],
+    "PSICOLOGIA": ["bg-purple-700", "text-white"],
+    "DIVIDENDOS": ["bg-yellow-700", "text-white"],
+    "EDUCACAO": ["bg-red-700", "text-white"],
+    "EMPREENDEDORISMO": ["bg-indigo-700", "text-white"],
+    "ACAO": ["bg-pink-700", "text-white"],
+    "FIIS": ["bg-teal-700", "text-white"]
+};
+
 const expirationMidnight = () => {
     const nextMidnight = new Date();
-    nextMidnight.setUTCHours(24, 0, 0, 0);
+    nextMidnight.setDate(nextMidnight.getDate() + 1);
+    nextMidnight.setUTCHours(0, 0, 0, 0);
     return nextMidnight.getTime();
 };
+
+const toggleQuoteDetails = (hidden) => {
+    quoteAuthor.hidden = hidden;
+    quoteSource.hidden = hidden;
+    quoteTags.hidden = hidden;
+}
 
 export async function getQuote(endpoint = "today", params = {}) {
     try {
@@ -30,6 +52,8 @@ export async function getQuote(endpoint = "today", params = {}) {
             if (cached) {
                 const { data, expiresAt } = JSON.parse(cached);
 
+                console.log("Frase Diária carregada do cache.");
+                console.log("Expira em:", new Date(expiresAt).toLocaleString());
                 if (Date.now() < expiresAt) {
                     return data;
                 }
@@ -62,52 +86,77 @@ export async function getQuote(endpoint = "today", params = {}) {
 
 export async function displayQuote(endpoint = "today") {
     const req = await getQuote(endpoint);
+
+    if (!req || req.length === 0) {
+        quoteText.textContent = "Não foi possível carregar a frase.";
+        toggleQuoteDetails(true);
+        return;
+    }
+
     const quote = req[0];
+    quoteTags.innerHTML = "";
 
     if (quote) {
         quoteText.textContent = quote.content;
         quoteAuthor.textContent = `~ ${quote.author}`;
-        quoteAuthor.hidden = false;
+        quoteSource.textContent = quote.source;
+
+        for (const tag of quote.tags) {
+            const tagElement = document.createElement("span");
+            const [bg, text] = validQuotesTags[tag] ?? ["bg-gray-700", "text-white"];
+
+            tagElement.className = `${bg} ${text} px-2 py-1 rounded-lg text-sm text-center font-semibold border border-black/10 dark:border-white/10`;
+            tagElement.textContent = tag;
+
+            quoteTags.appendChild(tagElement);
+        }
+
+        toggleQuoteDetails(false);
     } else {
         quoteText.textContent = "Não foi possível carregar a frase.";
-        quoteAuthor.hidden = true;
+        toggleQuoteDetails(true);
     }
+}
+
+function setActiveLink(active, inactive) {
+    active.classList.add("text-cyan-600", "dark:text-cyan-500");
+    active.classList.remove("text-gray-800", "dark:text-gray-200");
+
+    inactive.classList.add("text-gray-800", "dark:text-gray-200");
+    inactive.classList.remove("text-cyan-600", "dark:text-cyan-500");
 }
 
 todayQuoteLink.addEventListener("click", (e) => {
     e.preventDefault();
 
-    todayQuoteLink.classList.remove("text-gray-800", "dark:text-gray-200");
-    todayQuoteLink.classList.add("text-cyan-600", "dark:text-cyan-500");
-    randomQuoteLink.classList.remove("text-cyan-600", "dark:text-cyan-500");
-    randomQuoteLink.classList.add("text-gray-800", "dark:text-gray-200");
+    setActiveLink(todayQuoteLink, randomQuoteLink);
 
-    randomQuoteButton.classList.add("hidden");
+    randomQuoteButton.hidden = true;
     quoteTitle.textContent = "Frase Diária";
     quoteText.textContent = "Carregando Frase Diária...";
+    toggleQuoteDetails(true);
     displayQuote("today");
 });
 
 randomQuoteLink.addEventListener("click", (e) => {
     e.preventDefault();
 
-    randomQuoteLink.classList.add("text-cyan-600", "dark:text-cyan-500");
-    randomQuoteLink.classList.remove("text-gray-800", "dark:text-gray-200");
-    todayQuoteLink.classList.add("text-gray-800", "dark:text-gray-200");
-    todayQuoteLink.classList.remove("text-cyan-600", "dark:text-cyan-500");
+    setActiveLink(randomQuoteLink, todayQuoteLink);
 
-    randomQuoteButton.classList.remove("hidden");
+    randomQuoteButton.hidden = false;
     quoteTitle.textContent = "Frase Aleatória";
     quoteText.textContent = "Carregando Frase Aleatória...";
+    toggleQuoteDetails(true);
     displayQuote("random");
 });
 
 
 randomQuoteButton.addEventListener("click", () => {
-    if (randomQuoteButton.classList.contains("hidden")) {
+    if (randomQuoteButton.hidden) {
         return;
     }
 
     quoteText.textContent = "Carregando Frase Aleatória...";
+    toggleQuoteDetails(true);
     displayQuote("random");
 });
