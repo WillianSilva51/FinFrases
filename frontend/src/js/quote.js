c359e854399eb66ab786ed65f169c3c996fef598
Willian Silva <williansilva@alu.ufc.br>
2026-07-26 19:20:55 -0300

    feat: Update HTML structure for admin and search pages, and adjust API URL in quote.js

--- frontend/src/js/quote.js
+++ frontend/src/js/quote.js
@@ -1,5 +1,5 @@
-const API_URL = "https://finfrases.developer.li/api/v1/quotes/";
-// const API_URL = "https://localhost:8433/api/v1/quotes/"; localmente
+// const API_URL = "https://localhost:443/api/v1/quotes/"; localmente no docker
+const API_URL = "/api/v1/quotes/"; // caddy proxy
 
 const todayQuoteLink = document.getElementById("today-quote-link");
 const randomQuoteLink = document.getElementById("random-quote-link");
